bc8b937048e68686bc29c3b763ea87bab321ddee
Tani <tanishamehta3020@gmail.com>
2025-04-11 05:04:25 +0530

    Setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,10 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import './index.css'
-import {BrowserRouter} from 'react-router-dom'
-import StoreContextProvider from './context/StoreContext.jsx'
+import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <StoreContextProvider>
-      <App />
-    </StoreContextProvider> 
-  </BrowserRouter>
-    
-  
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
